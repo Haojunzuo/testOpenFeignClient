8a7a1cfeb14101c1230fca362d1dba99e6d07f7f
Junhao <2428393291@qq.com>
2022-03-13 21:39:19 +0800

    first commit

---
+++ src/main/java/com/example/testopenfeignclient/TestOpenFeignClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.testopenfeignclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestOpenFeignClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestOpenFeignClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testopenfeignclient/TestOpenFeignClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testopenfeignclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestOpenFeignClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
